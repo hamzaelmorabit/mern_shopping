8df76ada2a046e70a52be09f1be9a237ab751def
Hamza EL MORABIT <hamzamorabit123@gmail.com>
2021-11-01 17:51:53 +0100

    Deploy my app

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,6 @@
 module.exports = {
-  mongoURL: "mongodb://127.0.0.1:27017/mern-shopping",
+  // mongoURL: "mongodb://127.0.0.1:27017/mern-shopping",
+  mongoURL:
+    "mongodb+srv://hamza1997:hamza1997@contentkeeper.r11ch.mongodb.net/mern-shopping?retryWrites=true&w=majority",
 };
+
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const PORT = process.env.PORT || 5000;
 const mongoURL = require("./config/keys").mongoURL;
@@ -10,13 +11,19 @@ const items = require("./routes/api/items");
 // app.use(express.json());
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.send([{ user: 22 }]);
-});
+// app.get("/", (req, res) => {
+//   res.send([{ user: 22 }]);
+// });
 
 //
 app.use("/api/items", items);
-
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 mongoose.connect(mongoURL, { useUnifiedTopology: true }, (error, client) => {
   if (error) {
     console.log("Unable to connect to database");
