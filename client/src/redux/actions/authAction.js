3638bad97e4cfbec103ba0b894cb3a05e37dd8bb
Hamza EL MORABIT <hamzamorabit123@gmail.com>
2021-11-03 11:03:16 +0100

    Add End point auth/user to our server

---
+++ client/src/redux/actions/authAction.js
@@ -0,0 +1,54 @@
+import {
+  USER_LOADED,
+  USER_LOADING,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT_SUCCESS,
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+} from "../actions/types";
+import { returnErrors } from "./errorAction";
+import axios from "axios";
+
+export const loadUser = () => (dispatch, getState) => {
+  dispatch({
+    type: USER_LOADING,
+  });
+
+  axios
+    .get("/api/users/auth", tokenConfig(getState))
+    .then((res) => {
+      dispatch({
+        type: USER_LOADED,
+        payload: res.data,
+      });
+    })
+    .catch((err) => {
+      console.log(err.response, "err.response");
+      dispatch(returnErrors(err.response.data, err.response.status));
+      dispatch({
+        type: AUTH_ERROR,
+      });
+    });
+};
+
+// Setup config/headers and token
+export const tokenConfig = (getState) => {
+  // Get token from localstorage
+  const token = getState().auth.token;
+
+  // Headers
+  const config = {
+    headers: {
+      "Content-type": "application/json",
+    },
+  };
+
+  // If token, add to headers
+  if (token) {
+    config.headers["x-auth-token"] = token;
+  }
+
+  return config;
+};
